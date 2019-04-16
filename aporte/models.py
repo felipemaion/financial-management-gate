709c742ba894833cbfbafc5dcd9cf21e4605c488
Felipe Maion <felipe.maion@gmail.com>
2019-04-16 03:06:25 -0300

    Atualizado Grupo

--- aporte/models.py
+++ aporte/models.py
@@ -9,7 +9,7 @@ import json
 # Create your models here.
 
 class Grupo(models.Model):
-    name = models.CharField(unique=True)
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.name
