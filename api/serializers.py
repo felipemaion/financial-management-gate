46124c882085e3edc6922cf189a2f0a6a3965ed1
Felipe Maion <felipe.maion@gmail.com>
2019-04-17 03:14:38 -0300

    Implementado o consumo da API Aporte no Template

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
 
 from rest_framework import serializers
-from aporte.models import Aporte
+from aporte.models import Aporte, Grupo
 
 class AporteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aporte
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super(AporteSerializer, self).to_representation(instance)
+        data['selic'] = instance.amount_mais_selic() or None
+        data['grupo_name'] = instance.grupo.name or None
+        return data
+
+
--- app/urls.py
+++ app/urls.py
@@ -24,6 +24,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('',include('aporte.urls',namespace="aporte")),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
